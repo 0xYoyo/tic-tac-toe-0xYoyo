36920e977ac428df1da24cd264aae6a373678ec7
0xYoyo <113042560+0xYoyo@users.noreply.github.com>
2023-01-23 16:55:13 +0200

    add basic funtionality and structure

--- scripts.js
+++ scripts.js
@@ -1,10 +1,56 @@
 const gameBoard = (() => {
   const myBoard = [];
-  return { myBoard };
+  const len = 9;
+  const initBoard = () => {
+    for (let arrI = 0; arrI < len; arrI++) {
+      myBoard[arrI] = "";
+    }
+  };
+  return { myBoard, initBoard };
 })();
 
-const gameFlow = (player) => {};
-
 const playerMaker = (name, marker) => {
-  return { name, marker };
+  const score = 0;
+  const mark = (i) => {
+    gameBoard.myBoard[i] = marker;
+    console.log(gameBoard.myBoard[i]);
+  };
+  return { name, marker, mark, score };
 };
+
+const gameFlow = (() => {
+  gameBoard.initBoard();
+  const player1 = playerMaker("Player 1", "X");
+  const player2 = playerMaker("Player 2", "O");
+  let currentMarker = player1.marker;
+  console.log(currentMarker);
+  const confirmMarking = (index) => {
+    if (gameBoard.myBoard[index] === "") {
+      if (currentMarker === player1.marker) {
+        player1.mark(index);
+        currentMarker = player2.marker;
+      } else {
+        player2.mark(index);
+        currentMarker = player1.marker;
+      }
+    }
+  };
+  return { currentMarker, confirmMarking };
+})();
+
+const displayController = (() => {
+  const cells = document.querySelectorAll("li");
+  const renderBoard = () => {
+    cells.forEach((li) => {
+      const placement = li.getAttribute("data-");
+      li.textContent = gameBoard.myBoard[placement];
+    });
+  };
+  cells.forEach((li) => {
+    li.addEventListener("click", function () {
+      const placement = li.getAttribute("data-");
+      gameFlow.confirmMarking(placement);
+      renderBoard();
+    });
+  });
+})();
